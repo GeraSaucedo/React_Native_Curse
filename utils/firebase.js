1ca6ddf7063c77a223af18ef0416072bb6bd03b3
Gerardo Saucedo <jegsg@hotmail.es>
2018-12-26 19:29:47 -0600

    Delete firebase.js

--- utils/firebase.js
+++
@@ -1,8 +0,0 @@
-export default firebaseConfig = {
-    apiKey: "AIzaSyBSHY2OBkl_iBTorKFImBg_2n3ir7T72pA",
-    authDomain: "reactnativecurse.firebaseapp.com",
-    databaseURL: "https://reactnativecurse.firebaseio.com",
-    projectId: "reactnativecurse",
-    storageBucket: "",
-    messagingSenderId: "826738628034"
-}
