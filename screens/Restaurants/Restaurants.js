8f256491ebe8537fe58a078a5fc9924a2c0f3fb9
Gerardo Saucedo <jegsg@hotmail.es>
2019-01-09 19:44:47 -0600

    Search bar added

--- screens/Restaurants/Restaurants.js
+++ screens/Restaurants/Restaurants.js
@@ -1,11 +1,10 @@
 import React, { Component } from 'react';
 import PreLoader from '../../components/PreLoader';
-import AppButton from "../../components/AppButton";
 import t from 'tcomb-form-native';
-import { Card, ListItem } from 'react-native-elements';
+import { SearchBar, ListItem } from 'react-native-elements';
 const Form = t.form.Form;
 import * as  firebase from 'firebase';
-import { View, StyleSheet, FlatList } from 'react-native';
+import { StyleSheet, FlatList } from 'react-native';
 import BackgroundImage from '../../components/BackgroundImage';
 import RestaurantEmpty from '../../components/Restaurant/RestaurantEmpty'
 import RestaurantAddButton from '../../components/Restaurant/RestaurantAddButton'
@@ -17,13 +16,29 @@ export default class Restaurants extends Component {
         this.state = {
             restaurants: [],
             loaded: false,
-            restaurant_logo: require('../../assets/images/logo.jpg')
+            restaurant_logo: require('../../assets/images/logo.jpg'),
+            search: ''
         };
 
         this.refRestaurants = firebase.database().ref().child('restaurants');
     }
 
     componentDidMount () {
+        const {search} = this.state;
+
+        if ( ! search){
+            this.refRestaurants = firebase.database().ref().child('restaurants');
+        } else {
+            this.refRestaurants = firebase.database().ref().child('restaurants')
+                .orderByChild('name')
+                .startAt(search)
+                .endAt(`${search}\uf8ff`)
+        }
+
+        this._loadFirebaseRestaurants();
+    }
+
+    _loadFirebaseRestaurants () {
         this.refRestaurants.on('value',snapshot => {
             let restaurants = [];
             snapshot.forEach(row => {
@@ -71,34 +86,80 @@ export default class Restaurants extends Component {
         )
     }
 
+    searchRestaurants (search) {
+        this.setState({
+            search: search.charAt(0).toUpperCase() + search.slice(1)
+        });
+
+        if (search.length >= 3) { //buscara hasta que tse escriban mas de 3 caracteres
+            this._filterRestaurants(search);
+            setTimeout(() => {
+                this._loadFirebaseRestaurants();
+            },1000);
+        }
+
+        this._loadFirebaseRestaurants();
+    }
+
+    _filterRestaurants (search) {
+        this.refRestaurants = firebase.database().ref().child('restaurants')
+                .orderByChild('name')
+                .startAt(search)
+                .endAt(`${search}\uf8ff`);
+    }
+
+    resetSearch () {
+        this.setState({
+            search: ''
+        });
+        this.refRestaurants = firebase.database().ref().child('restaurants');
+        setTimeout(() => {
+            this._loadFirebaseRestaurants();
+        },1000);
+    }
+
     render () {
         const {loaded, restaurants} = this.state;
 
+        //Si no ha cargado
         if ( !loaded) {
             return <PreLoader/>
         };
 
+        const searchBar = (
+            <SearchBar
+            platform='android'
+            showLoading
+            cancelIcon={{type: 'font-awesome', name: 'chevron-left'}}
+            placeholder='Buscar restaurante'
+            onChangeText={(text) => this.searchRestaurants(text)}
+            onClear={this.resetSearch.bind(this)}
+            value={this.state.search}
+         />
+        )
+
+        //si no tenemos restaurantes
         if( !restaurants.length){
             
             return (
                 <BackgroundImage source={require('../../assets/images/bg2.jpg')}>
+                    {searchBar}
                     <RestaurantEmpty text="No hay restaurantes" />
                     <RestaurantAddButton addRestaurant={this.addRestaurant.bind(this)}/>
                 </BackgroundImage>
             );
         }
 
-        
+        //cuando ya existen restaurantes
         return (
             <BackgroundImage source={require('../../assets/images/bg2.jpg')}>
-                
-            <FlatList 
-                data={restaurants}
-                renderItem={(data) => this.renderRestaurant(data.item)}
-                keyExtractor={(data) => data.id}
-            />
-
-                <RestaurantAddButton addRestaurant={this.addRestaurant.bind(this)}/>
+                {searchBar}
+                <FlatList 
+                    data={restaurants}
+                    renderItem={(data) => this.renderRestaurant(data.item)}
+                    keyExtractor={(data) => data.id}
+                />
+                 <RestaurantAddButton addRestaurant={this.addRestaurant.bind(this)}/>
             </BackgroundImage>
         );
     }
